40e781917b9731c70432fdd18dd4c1a17a41fc09
victorvbello <victorvladi@gmail.com>
2019-10-22 04:31:38 -0300

    fix(api):confg env

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,14 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const EXTERNAL_API_PATH = 'https://simple.ripley.cl/api/v2/';
 export const REDIS_CONFIG = {
-  host: '127.0.0.1',
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 };
 export const FIREBASE_AUTH = {
-  apiKey: 'AIzaSyBS7hHtG2SolnAbWv0O346x7xqa-FdVrKQ',
-  authDomain: 'angularauth-156d2.firebaseapp.com',
-  databaseURL: 'https://angularauth-156d2.firebaseio.com',
-  projectId: 'angularauth-156d2',
-  storageBucket: 'angularauth-156d2.appspot.com',
-  messagingSenderId: '881628275923',
-  appId: '1:881628275923:web:9f02469e79842576862026',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATA_BASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
